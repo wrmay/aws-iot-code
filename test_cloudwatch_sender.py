d78d9f0cc7ed60121f259a822efd3d1fd80e215d
Randy May <randy@mathysphere.com>
2022-01-22 08:07:48 -0500

    Successful test of simple cloudwatch sender

--- test_cloudwatch_sender.py
+++ test_cloudwatch_sender.py
@@ -6,21 +6,20 @@ import time
 if __name__ == '__main__':
     cloudwatch = boto3.client('cloudwatch')
     while True:
+        metric_data = {
+                        'MetricName': 'response_time',
+                        'Dimensions': [
+                            {
+                                'Name': 'host',
+                                'Value': 'testhost'
+                            }
+                        ],
+                        'Timestamp': datetime.datetime.now(datetime.timezone.utc),
+                        'Value': random.gauss(10, 2),
+                        'Unit': 'Seconds',
+                        'StorageResolution': 1
+                    }
         cloudwatch.put_metric_data(Namespace='AWSIoTClass',
-                                   MetricData=[
-                                       {
-                                           'MetricName': 'response_time',
-                                           'Dimensions': [
-                                               {
-                                                   'Name': 'host',
-                                                   'Value': 'testhost'
-                                               }
-                                           ],
-                                           'Timestamp': datetime.datetime.now(),
-                                           'Value': random.gauss(10, 3),
-                                           'Unit': 'Seconds',
-                                           'StorageResolution': 1
-                                       }
-                                   ])
-        print('sent metric')
-        time.sleep(30)
+                                   MetricData=[metric_data])
+        print(f'sent {metric_data}')
+        time.sleep(10)
